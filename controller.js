4b15c4f0021689e6f0384bae2cbf9a0e48d07552
653sandeep <653sandeep@gmail.com>
2014-08-20 10:52:35 +0530

    Create controller.js

---
+++ controller.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose')
+var express = require('express');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var app = express();
+var bodyParser = require('body-parser');
+var url=require('url');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var TweetProvider = require('./testTweet').TweetProvider;
+var TweetProvider= new TweetProvider();
+
+
+  var server = app.listen(3001, function() {
+    console.log('Listening on port %d', server.address().port);
+  });
+
+
+  app.get('/tweet/', function(req, res){
+  TweetProvider.findAll(function(error, tweets){
+    res.write(JSON.stringify(tweets));
+    res.end();
+    });
+  })
+
+  app.post('/tweet/new', function(req, res){
+  TweetProvider.composeNew({
+    tHandle   : req.body.tHandle,
+    body      : req.body.body,
+  }, function(error, docs) {
+  res.redirect('/tweet/');
+  });
+});
